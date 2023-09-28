93bbe6775dfc62412a9d3cc52192a108acb60134
Gero <gtutos3@gmail.com>
2023-09-27 20:32:12 -0500

    Se agregaron algunas funciones de JavaScript y funcionamiento al formulario de contacto

---
+++ main.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal('.banner');
+ScrollReveal().reveal('.contact-container');
+ScrollReveal().reveal('.sobre-mi', { Delay: 500 });
+ScrollReveal().reveal('.about-cont', { Delay: 500 });
+ScrollReveal().reveal('.pefil-estudios-habilidades-experiencia', { Delay: 500 });
+ScrollReveal().reveal('.btn-wsp', { Delay: 500 });
